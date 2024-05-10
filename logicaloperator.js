3ef2fea08baf501c8d2bf5a92890d38c1b6a837f
Ali Bakhsh <160254256+alibakhsh7520@users.noreply.github.com>
2024-05-10 16:37:31 +0500

    first commit

---
+++ logicaloperator.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 2;
+// first condition and second condition dono theak to (true) true  = and operator
+let logicaland = (a > 0) && (b > 0);
+// koi see bhee ek condition (true) aur  second condition (false)) = true  = OR operator
+let logicalor = (a < 0) || (b > 0);
+// ulta oposit
+let logicalnot = (a > 0); // 5 > 0 = false
+console.log(logicaland);
+console.log(logicalor);
+console.log(logicalnot);
+export {};
---
+++ main.ts
@@ -0,0 +1,42 @@
+// //arithmetic operators\
+
+// let x:  number = 6;
+// let y: number = 8;
+
+// let sum: number = x+y;
+// console.log(sum);
+
+// let a: number = 8;
+// let b: number = 7;
+// let ab: number = a-b;
+// console.log(ab);
+
+// let c: number = 8;
+// let d: number = 5;
+// let cd: number = c*d;
+// console.log(cd);
+
+
+// let e: number = 3;
+// let f: number = 4;
+// let ef: number = e/f;
+// console.log(ef);
+
+// let g: number = 5;
+// let h: number = 9;
+// let gh: number = g%h;
+// console.log(gh);
+
+
+// let i: number = 8;
+// let j: number = 4;
+// let ij: number = ++i;
+// console.log(ij);
+
+// assignment operators
+let k: number = 10; // k = k+l  15
+let l: number = 5;  // l = k+l  5
+
+k +=l
+l +=k
+console.log(l)
